c10ae5c3af9fe99d39b60a1e8fcf0b58699cf477
John Coote <john_coote@rocketmail.com>
2018-07-10 23:37:26 +1000

    atbash

---
+++ atbash_ruby/main.rb
@@ -0,0 +1,25 @@
+require 'pry'
+
+class Atbash 
+
+    def initialize input  
+        @input = input
+        encode 
+    end 
+
+    def encode 
+        output = ""
+
+        alphabet = ("a".."z").to_a
+
+        @input.split("").each do |letter| 
+            index = alphabet.index letter
+            output << alphabet.reverse[index]
+        end
+
+        output
+    end 
+end 
+
+
+binding.pry 
