29ab0fc066c41bf483dce498b6eeec2f1b02b0fc
John Coote <john_coote@rocketmail.com>
2018-08-20 09:22:45 +1000

    word-soup

--- text_soup/js/main.js
+++ text_soup/js/main.js
@@ -1,17 +1,34 @@
 console.log("connected");
 
+let w = window.innerWidth;
+let h = window.innerHeight;
 
-const addWord = function(word="word") {
-  let $container = $('.container')
+const randy = (max) => {
+  return Math.floor( Math.random() * max)
+}
+
+
+
+const addWord = function(word = "word") {
+
+  let $words = $('#word-text-2').text().split(" ")
+  let randomIndex = randy( $words.length )
+  word = $words[randomIndex]
+
+
+  let $body = $('body')
   let $word = $(`<p>${word}</p>`).css({
-    color: "blue",
-    fontSize: "100px",
-    left: "20px",
-    top: "50px"
+    color: `rgb(${randy(255)}, ${randy(255)}, ${randy(255)})`,  //"white",
+    fontSize: (30 + randy(100)) + "px",  //"100px",
+    left: randy(w) + "px", //"20px",
+    top: randy(h) + "px", // "50px"
   })
 
-  $word.appendTo( $container )
-  // .fadeOut(2000, function() {
-    // $(this).remove()
-  // })
+  $word.appendTo( $body ).fadeOut(4000, function() {
+    $(this).remove()
+  })
 }
+
+
+
+setInterval(addWord, 400)
